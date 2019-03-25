b6c60f77812e2a277046dc65224c7de6600cb644
FlaviaBastos <flasanches@gmail.com>
2019-03-25 07:05:12 -0300

    Extract DB credentials to hidden file

--- project/server/config.py
+++ project/server/config.py
@@ -1,14 +1,15 @@
 # project/server/config.py
 
 import os
+from project.server import db_cred
 basedir = os.path.abspath(os.path.dirname(__file__))
-postgres_local_base = 'postgresql://postgres:@localhost/'
+postgres_local_base = f'postgresql://{db_cred.USER}:{db_cred.PASSWORD}@localhost/'
 database_name = 'flask_jwt_auth'
 
 
 class BaseConfig:
     """Base configuration."""
-    SECRET_KEY = 'my_precious'
+    SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious')
     DEBUG = False
     BCRYPT_LOG_ROUNDS = 13
     SQLALCHEMY_TRACK_MODIFICATIONS = False
