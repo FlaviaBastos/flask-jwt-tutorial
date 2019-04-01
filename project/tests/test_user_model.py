1c18f28bd494f25865d4a46a395c0beff1fbc95d
FlaviaBastos <flasanches@gmail.com>
2019-04-01 06:49:52 -0300

    Add decode token method and tests

--- project/tests/test_user_model.py
+++ project/tests/test_user_model.py
@@ -8,7 +8,7 @@ from project.tests.base import BaseTestCase
 
 class TestUserModel(BaseTestCase):
 
-    def test_encode_auth_toekn(self):
+    def test_encode_auth_token(self):
         user = User(
             email='test@test.com',
             password='test'
@@ -17,6 +17,17 @@ class TestUserModel(BaseTestCase):
         db.session.commit()
         auth_token = user.encode_auth_token(user.id)
         self.assertTrue(isinstance(auth_token, bytes))
+
+    def test_decode_auth_token(self):
+        user = User(
+            email='test@test.com',
+            password='test'
+        )
+        db.session.add(user)
+        db.session.commit()
+        auth_token = user.encode_auth_token(user.id)
+        self.assertTrue(isinstance(auth_token, bytes))
+        self.assertTrue(User.decode_auth_token(auth_token.decode("utf-8")) == 1)
 
 if __name__ == '__main__':
     unittest.main()
